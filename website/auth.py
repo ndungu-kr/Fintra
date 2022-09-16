3de43768daf15e6a5714a685fcb14c456f3a0010
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2022-09-16 17:17:29 +0100

    displaying correct links on nav bar

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
         else:
             flash("Email does not exist, please sign-up", category="error")
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
@@ -66,4 +66,4 @@ def sign_up():
             flash("Account Created.", category="success")
             return redirect(url_for("views.home"))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
