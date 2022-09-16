c5d47c1d1f63a5b844a8ef93020f5aaee073d8e8
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2022-09-16 17:13:11 +0100

    Added login management

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "SECRET@123"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
     from .views import views
@@ -18,13 +19,22 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from . import models
+    from .models import User, Transaction
 
     create_database(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
+
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists("website/" + DB_NAME):
         db.create_all(app=app)
-        print('Database created.')
+        print("Database created.")
