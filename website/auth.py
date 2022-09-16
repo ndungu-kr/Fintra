eaf3866d62a2d804e52ee53d4f44eb65b08dca8e
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2022-09-16 16:50:09 +0100

    SIgn up and login page functionality WIP

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully", category="success")
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect password, please try again", category="error")
+        else:
+            flash("Email does not exist, please sign-up", category="error")
+
     return render_template("login.html")
 
 
@@ -21,15 +38,27 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash("Email already exists.", category="error")
         if len(email) < 4:
             flash("Email must be greater than 4 characters", category="error")
         elif len(first_name) < 2:
             flash("First name must be greater than 1 characters", category="error")
-        elif password1 == password2:
+        elif password1 != password2:
             flash("Passwords do not match", category="error")
         elif len(password1) < 7:
             flash("Password must be at least 7 characters", category="error")
         else:
+            new_user = User(
+                email=email,
+                first_name=first_name,
+                password=generate_password_hash(password1, method="sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account Created.", category="success")
+            return redirect(url_for("views.home"))
 
     return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
+from email.policy import default
+from enum import unique
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.String(1000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    used_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    transactions = db.relationship('Transaction')
