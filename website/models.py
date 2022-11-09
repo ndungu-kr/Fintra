da4fda700ec85e3ebeb81c6fba419ee0bc89937c
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2022-11-09 18:05:58 +0000

    Updating Database with new tables

--- website/models.py
+++ website/models.py
@@ -3,14 +3,8 @@ from enum import unique
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
-
-class Transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Integer, default=0, nullable=False)
-    description = db.Column(db.String(1000), nullable=False)
-    date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+from sqlalchemy.orm import backref
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -18,12 +12,146 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    transactions = db.relationship("Transaction")
-    balance = db.relationship("Balance", back_populates="user")
+    wallet = db.relationship("Wallet", backref="user", uselist=False)
+    # transactions = db.relationship("Transaction") # one to many relationship (one user can have many transactions)
+    # balance = db.relationship("Balance", back_populates="user")
+
+    def __repr__(self):
+        return f"User('{self.first_name}', '{self.email}')"
 
 
-class Balance(db.Model):
+class Wallet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    total = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    user = db.relationship("User", back_populates="balance")
+    deposits = db.relationship("Deposit", backref="deposits", lazy=True)
+    withdrawals = db.relationship("Withdrawal", backref="withdrawals", lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True)
+    crypto_wallet = db.relationship("CryptoWallet", backref="wallet", uselist=False)
+    forex_wallet = db.relationship("ForexWallet", backref="wallet", uselist=False)
+    stock_wallet = db.relationship("StockWallet", backref="wallet", uselist=False)
+    total_balance = db.Column(db.DECIMAL, nullable=False, default=0)
+
+
+class Deposit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    wallet_id = db.Column(db.Integer, db.ForeignKey("wallet.id"), nullable=False)
+    amount = db.Column(db.DECIMAL, nullable=False)
+
+    def __repr__(self):
+        return f"Deposit('{self.id}', '{self.date}')"
+
+
+class Withdrawal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    wallet_id = db.Column(db.Integer, db.ForeignKey("wallet.id"), nullable=False)
+    amount = db.Column(db.DECIMAL, nullable=False)
+
+    def __repr__(self):
+        return f"Deposit('{self.id}', '{self.date}')"
+
+
+class Cryptocurrency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    value = db.Column(db.DECIMAL, nullable=False)
+    transactions = db.relationship(
+        "CryptocurrencyTransaction", backref="cryptocurrency", lazy=True
+    )
+
+
+class Currency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    value = db.Column(db.DECIMAL, nullable=False)
+    symbol = db.Column(db.String(5), nullable=False)
+    transactions = db.relationship("ForexTransaction", backref="currency", lazy=True)
+
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    value = db.Column(db.DECIMAL, nullable=False)
+    transactions = db.relationship("StockTransaction", backref="stock", lazy=True)
+
+
+class CryptoWallet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    transactions = db.relationship(
+        "CryptocurrencyTransaction", backref="transactions", lazy=True
+    )
+    wallet_id = db.Column(
+        db.Integer, db.ForeignKey("wallet.id"), unique=True, nullable=False
+    )
+    balance = db.Column(db.DECIMAL, nullable=False, default=0)
+
+
+class ForexWallet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    transactions = db.relationship(
+        "ForexTransaction", backref="transactions", lazy=True
+    )
+    wallet_id = db.Column(
+        db.Integer, db.ForeignKey("wallet.id"), unique=True, nullable=False
+    )
+    balance = db.Column(db.DECIMAL, nullable=False, default=0)
+
+
+class StockWallet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    transactions = db.relationship(
+        "StockTransaction", backref="transactions", lazy=True
+    )
+    wallet_id = db.Column(
+        db.Integer, db.ForeignKey("wallet.id"), unique=True, nullable=False
+    )
+    balance = db.Column(db.DECIMAL, nullable=False, default=0)
+
+
+class CryptocurrencyTransaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    crypto_wallet_id = db.Column(
+        db.Integer, db.ForeignKey("crypto_wallet.id"), nullable=False
+    )  # check here if relationships are messed up
+    cryptocurrency_id = db.Column(
+        db.Integer, db.ForeignKey("cryptocurrency.id"), nullable=False
+    )
+    crypto_amount = db.Column(db.NUMERIC, nullable=False)
+    rate = db.Column(db.NUMERIC, nullable=False)
+
+    def __repr__(self):
+        return f"Deposit('{self.id}', '{self.date}')"
+
+
+class ForexTransaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    forex_wallet_id = db.Column(
+        db.Integer, db.ForeignKey("forex_wallet.id"), nullable=False
+    )  # check here if relationships are messed up
+    currency_id = db.Column(db.Integer, db.ForeignKey("currency.id"), nullable=False)
+    currency_amount = db.Column(db.NUMERIC, nullable=False)
+    rate = db.Column(db.NUMERIC, nullable=False)
+
+    def __repr__(self):
+        return f"Deposit('{self.id}', '{self.date}')"
+
+
+class StockTransaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    stock_wallet_id = db.Column(
+        db.Integer, db.ForeignKey("stock_wallet.id"), nullable=False
+    )  # check here if relationships are messed up
+    stock_id = db.Column(db.Integer, db.ForeignKey("stock.id"), nullable=False)
+    stock_amount = db.Column(db.NUMERIC, nullable=False)
+    rate = db.Column(db.NUMERIC, nullable=False)
+
+    def __repr__(self):
+        return f"Deposit('{self.id}', '{self.date}')"
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from unicodedata import category
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Transaction
+
+# from .models import Transaction
 from . import db
 import json
 
@@ -12,23 +13,24 @@ views = Blueprint("views", __name__)
 @login_required
 def dashboard():
     if request.method == "POST":
-        description = request.form.get("description")
-        transaction_amount = request.form.get("transaction_amount")
+        pass
+        # description = request.form.get("description")
+        # transaction_amount = request.form.get("transaction_amount")
 
-        if len(description) < 1:
-            flash("Note is too short", category="error")
-        elif transaction_amount == 0:
-            flash("Please enter an amount", category="error")
-        else:
-            new_description = Transaction(
-                description=description,
-                user_id=current_user.id,
-                amount=transaction_amount,
-            )
-            db.session.add(new_description)
-            db.session.add()
-            db.session.commit()
-            flash("Transaction successfully added.", category="success")
+        # if len(description) < 1:
+        #     flash("Note is too short", category="error")
+        # elif transaction_amount == 0:
+        #     flash("Please enter an amount", category="error")
+        # else:
+        #     new_description = Transaction(
+        #         description=description,
+        #         user_id=current_user.id,
+        #         amount=transaction_amount,
+        #     )
+        #     db.session.add(new_description)
+        #     db.session.add()
+        #     db.session.commit()
+        #     flash("Transaction successfully added.", category="success")
     else:
         pass
     return render_template("dashboard.html", user=current_user)
@@ -36,12 +38,12 @@ def dashboard():
 
 @views.route("/delete-transaction", methods=["POST"])
 def delete_transaction():
-    transaction = json.loads(request.data)
-    transactionId = transaction["transactionId"]
-    transaction = Transaction.query.get(transactionId)
-    if transaction:
-        if transaction.user_id == current_user.id:
-            db.session.delete(transaction)
-            db.session.commit()
+    # transaction = json.loads(request.data)
+    # transactionId = transaction["transactionId"]
+    # transaction = Transaction.query.get(transactionId)
+    # if transaction:
+    #     if transaction.user_id == current_user.id:
+    #         db.session.delete(transaction)
+    #         db.session.commit()
 
     return jsonify({})
