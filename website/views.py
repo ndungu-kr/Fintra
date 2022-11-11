89e2b4561e2ccc6dd82848153fc93d8baf941e56
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2022-11-11 16:26:04 +0000

    Adding buy/sell asset pages

--- website/views.py
+++ website/views.py
@@ -55,16 +55,52 @@ def cryptocurrency_wallet():
     return render_template("cryptocurrency_wallet.html", user=current_user)
 
 
+@views.route("/cryptocurrency-wallet/buy-cryptocurrency", methods=["GET", "POST"])
+@login_required
+def buy_cryptocurrency():
+    return render_template("buy_cryptocurrency.html", user=current_user)
+
+
+@views.route("/cryptocurrency-wallet/sell-cryptocurrency", methods=["GET", "POST"])
+@login_required
+def sell_cryptocurrency():
+    return render_template("sell_cryptocurrency.html", user=current_user)
+
+
 @views.route("/forex-wallet", methods=["GET", "POST"])
 @login_required
 def forex_wallet():
     return render_template("forex_wallet.html", user=current_user)
 
 
+@views.route("/forex-wallet/buy-forex", methods=["GET", "POST"])
+@login_required
+def buy_forex():
+    return render_template("buy_forex.html", user=current_user)
+
+
+@views.route("/forex-wallet/sell-forex", methods=["GET", "POST"])
+@login_required
+def sell_forex():
+    return render_template("sell_forex.html", user=current_user)
+
+
 @views.route("/stock-wallet", methods=["GET", "POST"])
 @login_required
 def stock_wallet():
     return render_template("stock_wallet.html", user=current_user)
+
+
+@views.route("/stock-wallet/buy-stock", methods=["GET", "POST"])
+@login_required
+def buy_stock():
+    return render_template("buy_stock.html", user=current_user)
+
+
+@views.route("/stock-wallet/sell-stock", methods=["GET", "POST"])
+@login_required
+def sell_stock():
+    return render_template("sell_stock.html", user=current_user)
 
 
 @views.route("/cryptocurrency-tracker", methods=["GET", "POST"])
