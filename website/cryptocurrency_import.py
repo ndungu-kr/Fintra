a0d6f67cd2d69a6f34397002bbd312d10655f959
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2023-04-28 17:35:04 +0100

    advanced cryptocurrency import WIP

--- website/cryptocurrency_import.py
+++ website/cryptocurrency_import.py
@@ -1,50 +1,75 @@
 import requests
 import csv
 from os import path
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def crypto_import():
-    # Replace YOUR_API_KEY with your actual API key
     api_key = "175fea6e-3c78-4ea7-82af-7fbe9f03a6a5"
 
-    # Define the URL for the API endpoint
+    # URL for the API endpoint
     url = f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=500&convert=USD"
 
-    # Set the headers with your API key
+    # Setting header with API key
     headers = {"Accepts": "application/json", "X-CMC_PRO_API_KEY": api_key}
 
+    current_time = datetime.now(timezone.utc)
+    current_timestamp = current_time.timestamp()
+
+    # Removing milliseconds from time for csv
+    formatted_time = current_time.strftime("%Y-%m-%d %H%M%S")
+
+    filename = f"top_cryptocurrencies {formatted_time}.csv"
+
     # Check if the CSV file already exists
-    if path.exists("top500_cryptos.csv"):
-        print("Cryptocurrencies file already exists. Exiting script...")
-    else:
-        # Send a GET request to the API endpoint and retrieve the data
-        response = requests.get(url, headers=headers)
-        data = response.json()
+    if path.exists(filename):
+        file_timestamp = path.getmtime(filename)
+        time_diff = current_timestamp - file_timestamp
 
-        # Create a list of dictionaries containing the cryptocurrency data
-        cryptos = []
-        for crypto in data["data"]:
-            timestamp_str = crypto["last_updated"]
-            timestamp = datetime.strptime(
-                timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ"
-            ).isoformat()
-            cryptos.append(
-                {
-                    "code": crypto["symbol"],
-                    "name": crypto["name"],
-                    "current_price": crypto["quote"]["USD"]["price"],
-                    "last_updated": timestamp,
-                }
+        # Check if the CSV file is older than 7 minutes (420 seconds)
+        if time_diff < 420:
+            print("CSV file is up-to-date. Exiting script...")
+            return
+        else:
+            print(
+                "CSV file is older than 7 minutes. Calling API to create new CSV file..."
             )
+            # Move file to past details folder here
+            crypto_data_to_csv(url, headers, filename)
 
-        # Write the cryptocurrency data to a CSV file
-        with open("top500_cryptos.csv", "w", newline="") as csvfile:
-            fieldnames = ["code", "name", "current_price", "last_updated"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    else:
+        print("CSV file does not exist. Calling API to create new CSV file...")
+        crypto_data_to_csv(url, headers, filename)
 
-            writer.writeheader()
-            for crypto in cryptos:
-                writer.writerow(crypto)
+
+def crypto_data_to_csv(url, headers, filename):
+    # Retreiving data from API using a GET request
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # Creating a list of dictionaries containing the cryptocurrency data
+    cryptos = []
+    for crypto in data["data"]:
+        timestamp_str = crypto["last_updated"]
+        timestamp = datetime.strptime(
+            timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ"
+        ).isoformat()
+        cryptos.append(
+            {
+                "code": crypto["symbol"],
+                "name": crypto["name"],
+                "current_price": crypto["quote"]["USD"]["price"],
+                "last_updated": timestamp,
+            }
+        )
+
+    # Writing the cryptocurrency data onto a CSV file
+    with open(path.filename, "w", newline="") as csvfile:
+        fieldnames = ["code", "name", "current_price", "last_updated"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for crypto in cryptos:
+            writer.writerow(crypto)
 
         print("Cryptocurrency file created successfully.")
