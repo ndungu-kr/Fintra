a04edb21550fb256f1fb785e706e15644dcb95a9
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2023-06-16 16:18:14 +0100

    Removed unused code

--- website/views.py
+++ website/views.py
@@ -12,41 +12,7 @@ from website.cryptocurrency import *
 @views.route("/", methods=["GET", "POST"])
 @login_required
 def dashboard():
-    if request.method == "POST":
-        pass
-        # description = request.form.get("description")
-        # transaction_amount = request.form.get("transaction_amount")
-
-        # if len(description) < 1:
-        #     flash("Note is too short", category="error")
-        # elif transaction_amount == 0:
-        #     flash("Please enter an amount", category="error")
-        # else:
-        #     new_description = Transaction(
-        #         description=description,
-        #         user_id=current_user.id,
-        #         amount=transaction_amount,
-        #     )
-        #     db.session.add(new_description)
-        #     db.session.add()
-        #     db.session.commit()
-        #     flash("Transaction successfully added.", category="success")
-    else:
-        pass
     return render_template("dashboard.html", user=current_user)
-
-
-# @views.route("/delete-transaction", methods=["POST"])
-# def delete_transaction():
-#     # transaction = json.loads(request.data)
-#     # transactionId = transaction["transactionId"]
-#     # transaction = Transaction.query.get(transactionId)
-#     # if transaction:
-#     #     if transaction.user_id == current_user.id:
-#     #         db.session.delete(transaction)
-#     #         db.session.commit()
-
-#     return jsonify({})
 
 
 @views.route("/cryptocurrency-wallet/sell-cryptocurrency", methods=["GET", "POST"])
