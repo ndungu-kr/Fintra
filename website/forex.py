90b972180d6027d24bc857bce8156cf0dadfb1cd
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2023-06-22 11:54:06 +0100

    updating how forex balance is calculated

--- website/forex.py
+++ website/forex.py
@@ -96,7 +96,7 @@ def calc_asset_totals():
             asset_data = get_asset_data(asset.code)
 
             # add asset price and name to the object
-            asset.price = asset_data.current_price
+            asset.price = 1 / asset_data.current_price
             asset.name = asset_data.name
             asset.code = asset_data.code
 
