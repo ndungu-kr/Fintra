0c66792f606b53effc079283ea4ea9e6c3ddfd56
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2023-07-04 11:36:05 +0100

    Removing buy and sell asset pages

--- website/views.py
+++ website/views.py
@@ -7,45 +7,4 @@ views = Blueprint("views", __name__)
 from website.forex import *
 from website.stock import *
 from website.cryptocurrency import *
-
-
-@views.route("/", methods=["GET", "POST"])
-@login_required
-def dashboard():
-    return render_template("dashboard.html", user=current_user)
-
-
-@views.route("/cryptocurrency-wallet/buy-cryptocurrency", methods=["GET", "POST"])
-@login_required
-def buy_cryptocurrency():
-    return render_template("buy_cryptocurrency.html", user=current_user)
-
-
-@views.route("/cryptocurrency-wallet/sell-cryptocurrency", methods=["GET", "POST"])
-@login_required
-def sell_cryptocurrency():
-    return render_template("sell_cryptocurrency.html", user=current_user)
-
-
-@views.route("/forex-wallet/buy-forex", methods=["GET", "POST"])
-@login_required
-def buy_forex():
-    return render_template("buy_forex.html", user=current_user)
-
-
-@views.route("/forex-wallet/sell-forex", methods=["GET", "POST"])
-@login_required
-def sell_forex():
-    return render_template("sell_forex.html", user=current_user)
-
-
-@views.route("/stock-wallet/buy-stock", methods=["GET", "POST"])
-@login_required
-def buy_stock():
-    return render_template("buy_stock.html", user=current_user)
-
-
-@views.route("/stock-wallet/sell-stock", methods=["GET", "POST"])
-@login_required
-def sell_stock():
-    return render_template("sell_stock.html", user=current_user)
+from website.dashboard import *
